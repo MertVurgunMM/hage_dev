25c8e2692f1edc24b3c97c1d67cbd1edb28f262e
Mert Vurgun <vurgun@medienmonster.com>
2022-04-09 07:27:35 +0300

    introduce login unit tests

---
+++ app/src/test/java/com/practice/hagekotlin/LoginTest.kt
@@ -0,0 +1,41 @@
+package com.practice.hagekotlin
+
+import org.junit.Test
+
+class LoginTest {
+
+    @Test
+    fun test_loginWithCredentials_emptyFirstname_failed() {
+        assert(false)
+    }
+
+    @Test
+    fun test_loginWithCredentials_emptyLastname_failed() {
+        assert(false)
+    }
+
+    @Test
+    fun test_loginWithCredentials_emptyPersonalNumber_failed() {
+        assert(false)
+    }
+
+    @Test
+    fun test_loginWithCredentials_incorrectFirstname_failed() {
+        assert(false)
+    }
+
+    @Test
+    fun test_loginWithCredentials_incorrectLastname_failed() {
+        assert(false)
+    }
+
+    @Test
+    fun test_loginWithCredentials_incorrectPersonalNumber_failed() {
+        assert(false)
+    }
+
+    @Test
+    fun test_loginWithCredentials_correctCredentials_success() {
+        assert(false)
+    }
+}
